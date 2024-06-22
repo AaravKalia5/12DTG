7780a081624fc9c1284439795ac437afa05bbda1
Aarav Kalia <21202@wc.school.nz>
2024-06-23 11:41:26 +1200

    I removed all .html from the html links and made it /...

--- app.py
+++ app.py
@@ -7,17 +7,19 @@ app = Flask(__name__)
 def render_home_page():  # put application's code here
     return render_template('index.html')
 
-@app.route('/players.html')
+@app.route('/players')
 def render_players_page():  # put application's code here
     return render_template('players.html')
 
-@app.route('/stats.html')
+@app.route('/stats')
 def render_stats_page():  # put application's code here
     return render_template('stats.html')
 
-@app.route('/all.html')
+@app.route('/all')
 def render_all_page():  # put application's code here
     return render_template('all.html')
+
+
 
 
 if __name__ == '__main__':
