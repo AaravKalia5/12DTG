644dfae0556c9f3d93684eee7e3e1dda840e257b
Aarav Kalia <21202@wc.school.nz>
2024-06-23 13:13:39 +1200

    I have displayed all the data to the all page

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = 'cricket_database.db'
+
+def create_connection(db_filename):
+    try:
+        connection = sqlite3.connect(db_filename)
+        return connection
+
+    except Error as e:
+        print(e)
+        return None
 
 
 @app.route('/')
@@ -17,7 +29,18 @@ def render_stats_page():  # put application's code here
 
 @app.route('/all')
 def render_all_page():  # put application's code here
-    return render_template('all.html')
+    query = "SELECT Player, Country, Matches, Innings, Not_Outs, Runs, Ave, Balls_Faced, Strike_Rate, Hundreds ,Fiftys, Fours, Sixes, Ducks FROM cricket_table_data"
+
+    connection = create_connection(DATABASE)
+
+    cursor = connection.cursor()
+    cursor.execute(query, )
+
+    data_list = cursor.fetchall()
+
+
+
+    return render_template('all.html', data=data_list)
 
 
 
